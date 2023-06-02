901646f0aa1d485f17aefd8078beaae4c4142e39
hhkim331 <97862258+hhkim331@users.noreply.github.com>
2023-06-02 16:51:15 +0900

    정리
    정리

--- Assets/1.Scripts/Enemy/BulletTest.cs
+++ Assets/1.Scripts/Enemy/BulletTest.cs
@@ -50,21 +50,6 @@ public class BulletTest : CutterBehaviour
         }
     }
 
-    //public void Cut()
-    //{
-    //    var roots = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
-    //    foreach (var root in roots)
-    //    {
-    //        if (!root.activeInHierarchy)
-    //            continue;
-    //        var targets = root.GetComponentsInChildren<MeshTarget>();
-    //        foreach (var target in targets)
-    //        {
-    //            Cut(target, transform.position, transform.forward, null, OnCreated);
-    //        }
-    //    }
-    //}
-
     void OnCreated(Info info, MeshCreationData cData)
     {
         MeshCreation.TranslateCreatedObjects(info, cData.CreatedObjects, cData.CreatedTargets, Separation);
--- Assets/1.Scripts/Player/Bullet.cs
+++ Assets/1.Scripts/Player/Bullet.cs
@@ -1,27 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using DynamicMeshCutter;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bullet : CutterBehaviour
 {
-
-    
+    Collider coll;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        coll = GetComponent<Collider>();
+
+        //10초 뒤에 자기 자신을 파괴
+        Destroy(gameObject, 10f);
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+
+        //총알 앞으로 날라가게 하기
+        transform.position += transform.forward * Time.deltaTime * 10f;
     }
-    //충동감지
+
+    //충돌처리
     private void OnCollisionEnter(Collision collision)
     {
-        //파괴한다
-        Destroy(gameObject);
+        //충돌한 상대방 게임오브젝트의 태그값 비교
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Transform root = collision.transform.root;
+
+            var targets = root.GetComponentsInChildren<MeshTarget>();
+            foreach (var target in targets)
+            {
+                Cut(target, transform.position, transform.up, null, OnCreated);
+            }
+
+            ////충돌한 상대방 게임오브젝트 삭제
+            //Destroy(collision.gameObject);
+            //자기 자신도 삭제
+            //Destroy(gameObject);
+
+            //충돌 비활성화
+            coll.enabled = false;
+        }
+    }
+
+    void OnCreated(Info info, MeshCreationData cData)
+    {
+        MeshCreation.TranslateCreatedObjects(info, cData.CreatedObjects, cData.CreatedTargets, Separation);
     }
 }
