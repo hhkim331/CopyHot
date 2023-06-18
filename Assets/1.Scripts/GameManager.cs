13c807c6beac65b6112c9cff63e3e2a84517adf6
Leeginsu <135006635+Leeginsu@users.noreply.github.com>
2023-06-18 10:39:01 +0900

    시작멘트

--- Assets/1.Scripts/GameManager.cs
+++ Assets/1.Scripts/GameManager.cs
@@ -5,12 +5,36 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     public int stage;
+
+    public GameObject gameStartMassage;
+    bool gamestart = false;
+
     public TotalEnemySpawnData totalEnemySpawnData;
+    public GameObject slow;
+    float gametime;
+
     private void Start()
     {
         //화면 중앙에 커서 고정
         Cursor.lockState = CursorLockMode.Locked;
         //커서 보이지않음
         Cursor.visible = false;
+        gameStartMassage.SetActive(true);
+        Time.timeScale = 0;
+        gamestart = true;
     }
+
+    private void Update()
+    {
+        if (gamestart == true)
+        {
+            gametime += Time.unscaledTime;
+            if (gametime > 3000)
+            {
+                gameStartMassage.SetActive(false);
+                slow.SetActive(true);
+            }
+        }
+    }
+
 }
